eef38cbecc9e8530a4d4ac5dcf249a34f364d896
promitanan <promitanan@gmail.com>
2016-04-19 17:22:39 -0700

    Unit test passed

---
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -0,0 +1,28 @@
+package tests;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.android.lab3.MainActivity;
+import com.android.lab3.R;
+
+/**
+ * Created by Promita on 4/19/2016.
+ */
+
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity mainActivity;
+
+    public JUnit_test(){
+
+        super(MainActivity.class);
+    }
+
+    public void test_first() {
+
+        mainActivity = getActivity();
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Hello World!", tester);
+    }
+}
